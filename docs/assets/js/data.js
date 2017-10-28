9e48d3776a5390411770b6e978ca0071c6e93573
Folkert-Jan van der Pol <folkertjanvdpol@gmail.com>
2017-10-28 16:42:38 +0200

    First test with real data

--- docs/assets/js/data.js
+++ docs/assets/js/data.js
@@ -1,4 +1,5 @@
 var Data = {}
+var resultContainer = document.querySelector("#resultContainer");
 
 
 function loadData(url, callback) {
@@ -16,7 +17,18 @@ function loadData(url, callback) {
 var url = window.location.href.split("/")
 url.pop();
 url = url.join("/") + "/assets/data/data.json";
-loadData(url, function(result){
+loadData(url, onLoad(data));
+
+
+var onLoad = function(result){
     Data.all = JSON.parse(result);
     console.log(Data.all.stories);
-});
+    var articles = [];
+    Data.all.stories.forEach(function(story){
+        story.link = story.title.split(" ").join("-").toLowerCase();
+        console.log(story.link);
+        var html = "<article><img src='https://unsplash.it/400/200' alt=''><ul><li>Like</li><li>Save</li><li>Download</li></ul><h3>"+story.title"+</h3>            <p>"+story.summary+"</p><a href='verhalen/"+story.link+".html'>Lees meer van '"+story.title+"'</a></article>";
+        articles.push(html);
+    });
+    resultContainer.innerHTML = articles.join(" ");
+}
