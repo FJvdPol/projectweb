f86f107f25fc5babc14dffc5209d21991d6edf1f
Folkert-Jan van der Pol <folkertjanvdpol@gmail.com>
2017-10-30 17:06:26 +0100

    made images scale better

--- docs/assets/js/overviewpages.js
+++ docs/assets/js/overviewpages.js
@@ -60,7 +60,7 @@ var onLoad = function(data){
     stories.forEach(function(story){
         story.link = story.title.split(" ").join("-").toLowerCase();
         console.log(story.link);
-        var html = "<article><img src='"+story.img+"' alt=''><ul><li><button aria-label='Like'></button></li><li><button aria-label='Opslaan' data-story='"+story.title.toLowerCase()+"'></button></li><li><button aria-label='Download verhaal'></button></li></ul><h3>"+story.title+"</h3><p>"+story.summary+"</p><a href='verhalen/"+story.link+".html'>Lees meer van '"+story.title+"'</a></article>";
+        var html = "<article><div><img src='"+story.img+"' alt=''></div><ul><li><button aria-label='Like'></button></li><li><button aria-label='Opslaan' data-story='"+story.title.toLowerCase()+"'></button></li><li><button aria-label='Download verhaal'></button></li></ul><h3>"+story.title+"</h3><p>"+story.summary+"</p><a href='verhalen/"+story.link+".html'>Lees meer van '"+story.title+"'</a></article>";
         articles.push(html);
     });
     resultContainer.innerHTML += articles.join(" ");
