2a9ee85161a1bede85da3c0b69441bd843ad1d3c
Folkert-Jan van der Pol <folkertjanvdpol@gmail.com>
2017-10-30 16:19:18 +0100

    Small fixes for datajs

--- docs/assets/js/overviewpages.js
+++ docs/assets/js/overviewpages.js
@@ -65,18 +65,16 @@ function articleActionButtons(){
     var savedPopup = document.querySelector("#popup");
     var actionButtons = document.querySelectorAll("main article button");
     if (actionButtons){
-        var account = JSON.parse(localStorage.getItem("curAcc"));
         actionButtons.forEach(function(button){
             if (button.dataset.story){
                 if (account.saved.indexOf(button.dataset.story.toLowerCase()) > -1) {
                     button.classList.add("active");
                 }
                 button.addEventListener("click", function(e){
-                    e.stopPropagation();
-                    e.stopImmediatePropagation();
                     if (!this.classList.contains("active")){
                         this.classList.add("active");
                         savedPopup.classList.add("active");
+                        var account = JSON.parse(localStorage.getItem("curAcc"));
                         if(account.saved){
                             account.saved.push(this.dataset.story);
                         } else {
@@ -90,6 +88,7 @@ function articleActionButtons(){
                     } else {
                         this.classList.remove("active");
                         savedPopup.classList.remove("active");
+                        var account = JSON.parse(localStorage.getItem("curAcc"));
                         var index = account.saved.indexOf(this.dataset.story);
                         if (index !== -1) {
                             account.saved.splice(index, 1);
